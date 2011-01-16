e045f5891af1a94d41512adfa1d1a3f46ea7f598
Sven Dahlstrand <sven.dahlstrand@gmail.com>
2011-01-16 19:36:15 +0100

    Add files for episode 002.

---
+++ 002-carousel/javascript.js
@@ -0,0 +1,10 @@
+var utils = request.getAttribute('sitevision.utils');
+var portletContextUtil = utils.getPortletContextUtil();
+var propertyUtil = utils.getPropertyUtil();
+var nodeTypeUtil = utils.getNodeTypeUtil();
+var imageRenderer = utils.getImageRenderer();
+
+var currentPage = portletContextUtil.getCurrentPage();
+var node = propertyUtil.getNode(currentPage, 'carousel');
+
+if (nodeTypeUtil.isImage(node)) { var folder = node.getParent().getNodes(); }
